bc1c6ff0dffa693da3fca348af9d8e74c91eda04
Joep Verhofstad <joepverhofstad@student.gildeopleidingen.nl>
2024-06-11 13:36:47 +0200

    Big Chungus

---
+++ src/BigChungus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bigchungus\Bigchungus;
+
+class BigChungus
+{
+    function BigChungus() {
+        for($i = 0; $i < 100; $i++) {
+            echo "<img src='https://static.wikia.nocookie.net/worldofmayhem_gamepedia_en/images/5/56/Big_Chungus_%28artwork%29.png/revision/latest/scale-to-width-down/1200?cb=20210416172417' width='100' height='100'>";
+            echo "<br>";
+        }
+    }
+}
+
+?>
